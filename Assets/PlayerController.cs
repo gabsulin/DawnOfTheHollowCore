d6150bec1ffd53b9cecc0a65569a78590baf4de1
gabsulin <gabrielhurda@gmail.com>
2025-10-07 13:02:26 +0200

    Initial commit

---
+++ Assets/PlayerController.cs
@@ -0,0 +1,83 @@
+using UnityEngine;
+using System.Collections.Generic;
+
+public class PlayerController : MonoBehaviour
+{
+    private Rigidbody2D rb;
+    private Animator animator;
+
+    public float moveSpeed;
+    public bool canMove = true;
+    public bool canAttack = true;
+
+    public bool isAttacking = false;
+    private float attackTimer = 0f;
+
+    public Vector2 input;
+    private Vector2 lastMovementDirection = Vector2.right;
+
+
+    private void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        animator = GetComponent<Animator>();
+    }
+
+    private void Update()
+    {
+        if (canMove) HandleMovement();
+
+        if (Input.GetKeyDown(KeyCode.C))
+        {
+            animator.SetBool("IsDead", true);
+            canMove = false;
+        }
+    }
+
+    private void HandleMovement()
+    {
+        input = new Vector2(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical")).normalized;
+
+        if (input.magnitude > 0)
+        {
+            transform.position += (Vector3)(input * moveSpeed * Time.deltaTime);
+            lastMovementDirection = input;
+            FlipCharacter(input.x);
+            animator.SetBool("IsMoving", true);
+        }
+        else
+        {
+            animator.SetBool("IsMoving", false);
+
+            if (!isAttacking)
+            {
+                FlipCharacter(lastMovementDirection.x);
+            }
+        }
+
+    }
+
+
+    private void FlipCharacter(float directionX)
+    {
+        Vector3 mouseWorldPos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+
+        if (mouseWorldPos.x > transform.position.x)
+        {
+            transform.localScale = new Vector3(1, 1, 1);
+        }
+        else if (mouseWorldPos.x < transform.position.x)
+        {
+            transform.localScale = new Vector3(-1, 1, 1);
+        }
+    }
+
+    
+
+    
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        rb.linearVelocity = Vector2.zero;
+    }
+}
---
+++ Assets/Scripts/Laser.cs
@@ -0,0 +1,103 @@
+using System;
+using UnityEngine;
+using System.Collections.Generic;
+
+public class Laser : MonoBehaviour
+{
+    public Camera cam;
+    public LineRenderer lineRenderer;
+    public Transform firePoint;
+    public GameObject startVFX;
+    public GameObject endVFX;
+    
+    private List<ParticleSystem> particles = new List<ParticleSystem>();
+    private Quaternion rotation;
+    void Start()
+    {
+        FillLists();
+        DisableLaser();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetButtonDown("Fire1"))
+        {
+            EnableLaser();
+        }
+        if(Input.GetButton("Fire1"))
+        {
+            UpdateLaser();
+        }
+        if(Input.GetButtonUp("Fire1"))
+        {
+            DisableLaser();
+        }
+        RotateToMouse();
+    }
+    void EnableLaser()
+    {
+        lineRenderer.enabled = true;
+
+        for (int i = 0; i < particles.Count; i++)
+        {
+            particles[i].Play();
+        }
+    }
+    private void UpdateLaser()
+    {
+        var mousePos = (Vector2)cam.ScreenToWorldPoint(Input.mousePosition);
+        lineRenderer.SetPosition(0, firePoint.position);
+        startVFX.transform.position = (Vector2)firePoint.position;
+
+        lineRenderer.SetPosition(1, mousePos);
+
+        Vector2 direction = mousePos - (Vector2)transform.position;
+        RaycastHit2D hit = Physics2D.Raycast((Vector2)transform.position, direction.normalized, direction.magnitude);
+
+        if(hit)
+        {
+            lineRenderer.SetPosition(1, hit.point);
+        }
+
+        endVFX.transform.position = lineRenderer.GetPosition(1);
+    }
+
+    private void DisableLaser()
+    {
+        lineRenderer.enabled = false;
+
+        for (int i = 0; i < particles.Count; i++)
+        {
+            particles[i].Stop();
+        }
+    }
+
+    void RotateToMouse()
+    {
+        Vector2 direction = cam.ScreenToWorldPoint(Input.mousePosition) - transform.position;
+        float angle = MathF.Atan2(direction.y, direction.x) * Mathf.Rad2Deg;
+        rotation.eulerAngles = new Vector3(0, 0, angle);
+        transform.rotation = rotation;
+    }
+
+    private void FillLists()
+    {
+        for (int i = 0; i < startVFX.transform.childCount; i++)
+        {
+            var ps = startVFX.transform.GetChild(i).GetComponent<ParticleSystem>();
+            if (ps != null)
+            {
+                particles.Add(ps);
+            }
+        }
+        for (int i = 0; i < endVFX.transform.childCount; i++)
+        {
+            var ps = endVFX.transform.GetChild(i).GetComponent<ParticleSystem>();
+            if (ps != null)
+            {
+                particles.Add(ps);
+            }
+        }
+    }
+}
